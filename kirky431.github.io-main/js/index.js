d6856ce51d088d25d90f6136361f53f63c864e76
Kirky431 <153141270+Kirky431@users.noreply.github.com>
2023-12-07 19:01:27 +0000

    Add files via upload

---
+++ kirky431.github.io-main/js/index.js
@@ -0,0 +1,28 @@
+function relevant_skills(){
+    var element = document.querySelector("#services")
+    element.scrollIntoView({behavior: "smooth"});
+}
+function Home(){
+    var element = document.querySelector("#home")
+    element.scrollIntoView({behavior: "smooth"});
+}
+function Education_and_training(){
+    var element = document.querySelector("#progression")
+    element.scrollIntoView({behavior: "smooth"});
+}
+function AboutMe(){
+    var element = document.querySelector("#about")
+    element.scrollIntoView({behavior: "smooth"});
+}
+function Employment_History_and_related_experiences(){
+    var element = document.querySelector("#projects")
+    element.scrollIntoView({behavior: "smooth"});
+}
+function EmailSubmit(){
+    location.href = "#home";
+    console.log("Email Submitting")
+}
+function Hobbies(){
+    var element = document.querySelector("#Hobbies")
+    element.scrollIntoView({behavior: "smooth"});
+}
